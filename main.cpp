f342a37144f34534ce80c5694a3b9fb76ab6a047
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-10-15 15:51:56 +0200

    main implementation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
+
+#include "boost/property_tree/json_parser.hpp"
 
 #include "algorithms.h"
 #include "parse.h"
 
 int main(int argc, const char *argv[]) {
-  std::cout << "property size: " << sizeof(EdgeProperty) << std::endl;
+  if (argc != 2) {
+    std::cout << "Usage: ./Main <input>" << std::endl;
+    std::exit(-1);
+  }
+  std::ifstream file(argv[1]);
+  PTree pt;
+  boost::property_tree::json_parser::read_json(file, pt);
+  Input input = parseInput(pt);
+  State state(input);
+  assignWithMinimumNumberPerGroup(state, 13);
+  printCurrentAssignment(state);
 }
