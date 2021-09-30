44d21e3a0f2e48f582b922278e3e2ac634d85cf0
Nikolai Maas <nikolai.maas@student.kit.edu>
2021-09-30 12:44:44 +0200

    update randomized test

--- randomized_test.cpp
+++ randomized_test.cpp
@@ -14,48 +14,53 @@ static std::mt19937 GENERATOR;
 GroupData createGroup(int &counter) {
   std::uniform_int_distribution<int> random(0, 9);
   bool is_math = random(GENERATOR) == 0;
-  bool is_master = random(GENERATOR) == 0;
   std::string name("Group ");
   name += std::to_string(++counter);
-  return GroupData(name, 18, is_math ? CourseType::Mathe : CourseType::Any,
-                   is_master ? DegreeType::Master : DegreeType::Any);
+  std::uniform_int_distribution<StudentID> capacity(15, 60);
+  StudentID cap = capacity(GENERATOR);
+  return GroupData(name, name, cap, is_math ? CourseType::Mathe : CourseType::Any);
 }
 
 StudentData createErsti(int &counter) {
-  std::uniform_int_distribution<int> random(0, 9);
-  bool is_math = random(GENERATOR) == 0;
-  bool is_master = random(GENERATOR) == 0;
+  std::uniform_int_distribution<int> random(0, 19);
+  int course_rand = random(GENERATOR);
+  CourseType course = course_rand == 0 ? CourseType::Lehramt : (
+    course_rand < 3 ? CourseType::Mathe : CourseType::Info
+  );
+  bool is_master = random(GENERATOR) < 2;
+  bool is_ersti = random(GENERATOR) < 16;
   std::string name("Ersti ");
   name += std::to_string(++counter);
-  return StudentData(name, is_math ? CourseType::Mathe : CourseType::Info,
+  return StudentData(name, name, course,
                      is_master ? DegreeType::Master : DegreeType::Bachelor,
-                     false);
+                     is_ersti ? Semester::Ersti : Semester::Dritti);
 }
 
-Rating createRating() {
-  std::uniform_int_distribution<int> random(0, 4);
-  return Rating(random(GENERATOR));
+std::vector<Rating> createRating(GroupID num_groups) {
+  std::vector<Rating> result;
+  for (GroupID i = 0; i < num_groups; ++i) {
+    result.push_back(i);
+  }
+  std::shuffle(result.begin(), result.end(), GENERATOR);
+  return result;
 }
 
 int main() {
   Input input;
-  std::uniform_int_distribution<int> random(2, 5);
-  int num_groups = 70;
-  int num_students = 1000;
-  int num_teams = 80;
+  std::uniform_int_distribution<int> random(2, 3);
+  int num_students = 600;
+  int num_teams = 50;
 
   int group_counter = 0;
-  while (group_counter < num_groups) {
+  int capacity = 0;
+  while (capacity < 5 * num_students / 4) {
     input.groups.push_back(createGroup(group_counter));
+    capacity += input.groups.back().capacity;
   }
   int student_counter = 0;
   while (student_counter < num_students) {
     input.students.push_back(createErsti(student_counter));
-    std::vector<Rating> rating;
-    for (int i = 0; i < num_groups; ++i) {
-      rating.push_back(createRating());
-    }
-    input.ratings.push_back(std::move(rating));
+    input.ratings.push_back(createRating(input.groups.size()));
   }
   StudentID student_id = 0;
   for (int team_counter = 0; team_counter < num_teams; ++team_counter) {
@@ -72,10 +77,28 @@ int main() {
   }
 
   State s(input);
-  assignWithMinimumNumberPerGroup(s, 5);
+  assignWithMinimumNumberPerGroup(s, MIN_GROUP_SIZE);
   auto is_math_and_no_ma = [](const StudentData &data) {
     return data.course_type == CourseType::Mathe &&
            data.degree_type != DegreeType::Master;
   };
-  assertMininumNumber(s, 4, is_math_and_no_ma);
+  auto is_lehramt_and_no_ma = [](const StudentData &data) {
+    return data.course_type == CourseType::Lehramt &&
+           data.degree_type != DegreeType::Master;
+  };
+  auto is_dritti_and_no_ma = [](const StudentData &data) {
+    return data.semester == Semester::Dritti &&
+           data.degree_type != DegreeType::Master;
+  };
+  auto is_master = [](const StudentData &data) {
+    return data.degree_type == DegreeType::Master;
+  };
+
+  std::cout << std::endl << "Reassingning specific students." << std::endl;
+  assertMinimumNumberPerGroupForSpecificType(s, {
+    {is_math_and_no_ma, 5, "Mathe (nicht MA)"},
+    {is_lehramt_and_no_ma, 5, "Lehramt (nicht MA)"},
+    {is_dritti_and_no_ma, 7, "Dritti (nicht MA)"},
+    {is_master, 5, "Master"}
+  });
 }
