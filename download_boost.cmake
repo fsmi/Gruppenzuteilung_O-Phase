6fcf320917b67415df58ef071acda195a81c8b11
Nikolai Maas <nikolai.maas@kit.edu>
2023-08-09 22:49:54 +0200

    update boost version

--- download_boost.cmake
+++ download_boost.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Populate(
   boost-src
-  URL http://downloads.sourceforge.net/project/boost/boost/1.74.0/boost_1_74_0.tar.bz2
-  URL_HASH MD5=da07ca30dd1c0d1fdedbd487efee01bd
+  URL https://sourceforge.net/projects/boost/files/boost/1.82.0/boost_1_82_0.tar.bz2/download
+  URL_HASH MD5=b45dac8b54b58c087bfbed260dbfc03a
   SOURCE_DIR boost
 )
